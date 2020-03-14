91e0a6c51b7609783ee0cac1c2e257822beb0ce3
Mikhail <nikitin.mikhail@mail.ru>
2020-03-15 01:19:03 +0400

    add FSM

--- i2c/rtl/i2c.v
+++ i2c/rtl/i2c.v
@@ -3,6 +3,7 @@ module i2c(
     input       reset,
     input [6:0] addr,
     input       rdWr,
+    input       startTxRx,
     input [7:0] inData,
     input       inValid,
     output      inReady,
@@ -15,10 +16,90 @@ module i2c(
     inout       scl
 );
 
-wire i2c_oe, sda_in, sda_out, scl_in, scl_out;
+wire i2c_oe, sdaIn, sda_out, sclIn, sclOut;
 
 assign sda = (sda_out | ~i2c_oe)    ? 1'bz : 1'b0;
-assign scl = scl_out                ? 1'bz : 1'b0;
-assign sda_in = sda;
+assign scl = sclOut                 ? 1'bz : 1'b0;
+assign sdaIn = sda;
+assign sclIn = scl;
 
-localparam stateStart = 0, stateStop = 1, state
+localparam  stateIdle = 0, stateStart = 1, stateAddr = 2, stateAck = 3, 
+            stateNack = 4, stateTx = 5, stateRx = 6, stateStop = 7;
+
+reg [2:0] state;
+wire transactionStart, startDone, addrDone, ackOk, rdwrOp, rxDone, txDone, stopDone;
+always @(posedge clock or posedge reset) begin
+    if (reset)
+        state <= stateIdle;
+    else begin
+        case (state)
+            stateIdle:  if (transactionStart)
+                            state <= stateStart;
+                        else
+                            state <= stateIdle;
+
+            stateStart: if (startDone)
+                            state <= stateAddr;
+                        else
+                            state <= stateStart;
+
+            stateAddr:  if (addrDone)
+                            if (ackOk)
+                                state <= stateAck;
+                            else
+                                state <= stateNack;
+                        else
+                            state <= stateAddr;
+
+            stateAck:   if (transactionDone)
+                            state <= stateStop;
+                        else
+                            if (rdwrOp)
+                                state <= stateRx;
+                            else
+                                state <= stateTx;
+
+            stateNack:  state <= stateStop;
+
+            stateRx:    if (rxDone)
+                            if (ackOk)
+                                state <= stateAck;
+                            else
+                                state <= stateNack;
+                        else
+                            state <= stateRx;
+
+            stateTx:    if (txDone)
+                            if (ackOk)
+                                state <= stateAck;
+                            else
+                                state <= stateNack;
+                        else
+                            state <= stateTx;
+            
+            stateStop:  if (stopDone)
+                            state <= stateIdle;
+                        else
+                            state <= stateStop;
+        endcase
+    end
+end
+
+//*****scl generation*****
+reg [9:0] cntScl;                       //For clock = 100 MHz, scl = 100/1024 MHz
+always @(posedge clock) begin
+    if (state == stateIdle)
+        cntScl <= 10'd0;
+    else    
+        if (~sclOut | sclIn)            //Check that the pin scl is "1"
+            cntScl <= cntScl + 10'd1;
+        else
+            cntScl <= cntScl;
+end
+assign sclOut = ~cntScl[9];
+assign transactionStart = (stobeStart & sdaIn & sclIn);
+assign startDone = ((state == stateStart) & ~sclIn);
+assign addrDone = (cntAddr == 4'd9);
+assign 
+
+endmodule
